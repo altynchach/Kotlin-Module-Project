da2d9b897751c4bd4db58931305b133038d2239c
altynchach <77624594+altynchach@users.noreply.github.com>
2024-04-24 20:12:01 +0600

    finished

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,19 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.util.Scanner
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    val menuManager = MenuManager(scanner)
+    val noteManager = NoteManager(menuManager)
+    val archiveManager = ArchiveManager(menuManager, noteManager)
+    var isRunning = true
+
+    while (isRunning) {
+        val mainMenu = listOf("Выбор архива", "Выход из приложения")
+        val choice = menuManager.showMenu(mainMenu, "Главный экран:")
+        when (choice) {
+            1 -> archiveManager.selectArchive()
+            2 -> isRunning = false
+        }
+    }
 }
+
